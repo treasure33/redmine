b52f691daf071a1829b2d741d0042f339b2dbf69
Ilia Kondrashov <caseycs@gmail.com>
2017-06-09 18:58:48 +0200

    add notes field to leave comment next to issue change

--- redmine.go
+++ redmine.go
@@ -315,6 +315,7 @@ type Issue struct {
 	Attachments    []*Attachment     `json:"attachments"`
 	Journals       []*IssueJournal   `json:"journals"`
 	Changesets     []*IssueChangeset `json:"changesets"`
+	Notes          string            `json:"notes"` // allows to leave comment next to changes
 	// TODO: watchers (since 2.3)
 }
 
@@ -381,6 +382,7 @@ type issue struct {
 	CategoryId     int            `json:"category_id,omitempty"`
 	CustomFields   []*customField `json:"custom_fields,omitempty"`
 	Uploads        []*Upload      `json:"uploads,omitempty"`
+	Notes          string         `json:"notes,omitempty"`
 }
 
 func (r *Issue) toSend(uploads []*Upload) *issue {
@@ -429,6 +431,7 @@ func (r *Issue) toSend(uploads []*Upload) *issue {
 		}
 	}
 	newIssue.Uploads = uploads
+	newIssue.Notes = r.Notes
 	return newIssue
 }
 
